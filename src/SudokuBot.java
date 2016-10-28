46afea7667d7cea90db0b7721bbc45ec2700ef9a
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2016-10-28 14:04:33 +0200

    Fixed DFS solution

--- src/SudokuBot.java
+++ src/SudokuBot.java
@@ -14,8 +14,7 @@ public class SudokuBot {
 
 	private static String[][] grid = new String[9][9];
 	private static ArrayList<String> placements = new ArrayList<String>();
-	private static Stack stack = new Stack();
-	private static Queue queue = new LinkedList<>();
+	private static boolean gameOver = false;
 
 	public static void readInput(String fileName) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
@@ -158,35 +157,125 @@ public class SudokuBot {
 		return possibleMoves;
 	}
 
-	public static void solveDepthFirst(int x, int y, String[][] newGrid) {
-		if (!newGrid[y][x].equals("*")) {
-			if (x == 8 && y == 8) {
-				return;
+	public static boolean isGameOver(String[][] newGrid) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (newGrid[i][j].equals("*")) {
+					return false;
+				}
 			}
-			if (x == 8) {
-				solveDepthFirst(0, y + 1, newGrid);
-			} else {
-				solveDepthFirst(x + 1, y, newGrid);
-			}
+		}
+		return true;
+	}
 
-		} else {
-			ArrayList<String> possibleMoves = possibleMoves(y, x, newGrid);
-			if (possibleMoves.isEmpty()) {
-				return;
+	public static int[] getFirstEmptyCell(String[][] currentState) {
+		int[] index = new int[2];
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (grid[i][j].equals("*")) {
+					index[0] = i;
+					index[1] = j;
+					return index;
+				}
 			}
-			for (int i = possibleMoves.size() - 1; i >= 0; i--) {
-				stack.push(possibleMoves.get(i));
+		}
+		return null;
+	}
+	
+	public static boolean checkGoal(String[][] grid) {
+		// For Full Board Checking
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (grid[i][j].equals("*")) {
+//					System.out.println("stars");
+					return false;
+				}
 			}
-			while(!stack.isEmpty()) {
-				newGrid[y][x] = stack.pop() + "";
-				if (x == 8 && y == 8) {
-					grid = newGrid;
+		}
+		// For Row Checking
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				for (int j2 = 0; j2 < 9; j2++) {
+					if (j != j2) {
+						if (grid[i][j].equals(grid[i][j2])) {
+//							System.out.println("row");
+//							System.out
+//									.println("[" + i + "]" + "[" + j + "]"
+//											+ " with " + "[" + i + "]" + "["
+//											+ j2 + "]");
+							return false;
+						}
+					}
+				}
+			}
+		}
+		// For Column Checking
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				for (int j3 = 0; j3 < 9; j3++) {
+					if (j != j3) {
+						if (grid[j][i].equals(grid[j3][i])) {
+//							System.out.println("column");
+//							System.out
+//									.println("[" + j + "]" + "[" + i + "]"
+//											+ " with " + "[" + j3 + "]" + "["
+//											+ i + "]");
+							return false;
+						}
+					}
+				}
+			}
+		}
+		// For Box Checking
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				int boxRowOffset = (i / 3) * 3;
+				int boxColOffset = (j / 3) * 3;
+				for (int k = 0; k < 3; k++) {
+					for (int m = 0; m < 3; m++) {
+						if ((boxRowOffset + k) != i || (boxColOffset + m) != j) {
+							if (grid[i][j]
+									.equals(grid[boxRowOffset + k][boxColOffset
+											+ m])) {
+//								System.out.println("box");
+//								System.out.println("[" + i + "]" + "[" + j
+//										+ "]" + " with " + "["
+//										+ (boxRowOffset + k) + "]" + "["
+//										+ (boxColOffset + m) + "]");
+								return false;
+							}
+						}
+					}
+				}
+
+			}
+		}
+
+		return true;
+	}
+
+	
+	public static void dfs(String[][] newGrid) {
+		if(!gameOver) {
+			int[] index = getFirstEmptyCell(newGrid);
+			if (index == null) {
+				if(checkGoal(newGrid)) {
+					grid=newGrid;
+					writeResult();
+					System.out.println("Done!!");
+					gameOver=true;
 					return;
 				}
-				if (x == 8) {
-					solveDepthFirst(0, y + 1, newGrid);
-				} else {
-					solveDepthFirst(x + 1, y, newGrid);
+				return;
+			}
+			int x = index[0];
+			int y = index[1];
+			for (int i = 1; i <= 10; i++) {
+				if(i<=9) {
+					newGrid[x][y] = i + "";
+					dfs(newGrid);
+				}else {
+					newGrid[x][y] ="*";
 				}
 			}
 		}
@@ -194,11 +283,10 @@ public class SudokuBot {
 
 	public static void main(String[] args) {
 		try {
-			readInput("input_example.txt");
+			readInput("input_example2.txt");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		solveDepthFirst(0, 0, grid);
-		writeResult();
+		dfs(grid);
 	}
 }
